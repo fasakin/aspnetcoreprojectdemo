883fd749a02a19499a3d28bfac599429b2428be4
ifedayo F Olaobi <fasakin.ifedayo@gmail.com>
2025-08-01 22:43:38 -0500

    models defined

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace NWebApp.Models;
+
+public class Product
+{
+    public long ProductId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public long CategoryId { get; set; }
+    public Category? Category { get; set; }
+    public long SupplierId { get; set; }
+    public Supplier? Supplier { get; set; }
+}
--- obj/Debug/net8.0/NWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/NWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab582829559e85883b159eba7051f5dd1028568d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
